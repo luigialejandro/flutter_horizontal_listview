2f1a7736ae68603c95223a3b82754faf2740cf40
luigialejandro <50028217+luigialejandro@users.noreply.github.com>
2022-01-20 09:10:28 -0600

    Creation of CardItem List which feeds buildCard method, ListView.separated is also incorporated

--- lib/main.dart
+++ lib/main.dart
@@ -20,6 +20,18 @@ class MyApp extends StatelessWidget {
   }
 }
 
+class CardItem {
+  final String downloadedImage;
+  final String title;
+  final String subtitle;
+
+  const CardItem({
+    required this.downloadedImage,
+    required this.title,
+    required this.subtitle,
+  });
+}
+
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
 
@@ -30,6 +42,29 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  List<CardItem> items = [
+    CardItem(
+      downloadedImage: "assets/images/comunicado0.png",
+      title: 'Nike Free Run',
+      subtitle: '\$99',
+    ),
+    CardItem(
+      downloadedImage: "assets/images/comunicado1.png",
+      title: 'Nike Air Jordan',
+      subtitle: '\$75',
+    ),
+    CardItem(
+      downloadedImage: "assets/images/comunicado2.png",
+      title: 'Nike Free Run',
+      subtitle: '\$99',
+    ),
+    CardItem(
+      downloadedImage: "assets/images/comunicado3.png",
+      title: 'Nike Free Run',
+      subtitle: '\$99',
+    ),
+  ];
+
   @override
   Widget build(BuildContext context) => Scaffold(
         appBar: AppBar(
@@ -37,40 +72,38 @@ class _MyHomePageState extends State<MyHomePage> {
           centerTitle: true,
         ),
         body: Container(
-          height: 200,
-          child: SingleChildScrollView(
+          height: 256,
+          child: ListView.separated(
             scrollDirection: Axis.horizontal,
-            child: Row(
-              children: [
-                buildCard(),
-                SizedBox(width: 12),
-                buildCard(),
-                SizedBox(width: 12),
-                buildCard(),
-                SizedBox(width: 12),
-                buildCard(),
-              ],
-            ),
+            itemCount: 4,
+            separatorBuilder: (context, _) => SizedBox(width: 12),
+            itemBuilder: (context, index) => buildCard(item: items[index]),
           ),
         ),
       );
 
-  Widget buildCard() => Container(
+  Widget buildCard({
+    required CardItem item,
+  }) =>
+      Container(
         width: 200,
-        height: 200,
-        color: Colors.red[200],
         child: Column(
           children: [
-            Image.asset("assets/images/comunicado0.png"),
+            Expanded(
+              child: Image.asset(
+                item.downloadedImage,
+                fit: BoxFit.cover,
+              ),
+            ),
             const SizedBox(
               height: 4,
             ),
             Text(
-              'Shoe XYZ',
-              style: TextStyle(fontSize: 20, color: Colors.black),
+              item.title,
+              style: TextStyle(fontSize: 24, color: Colors.black),
             ),
             Text(
-              '€90',
+              item.subtitle,
               style: TextStyle(fontSize: 20, color: Colors.black),
             ),
           ],
